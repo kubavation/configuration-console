55d9e23bc864b5ecab4d75c57b0613d8a3eb17d7
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-12 16:27:57 +0100

    init

---
+++ src/main/java/com/durys/jakub/configurationconsole/ConfigurationConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.configurationconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durys/jakub/configurationconsole/ConfigurationConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.configurationconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
