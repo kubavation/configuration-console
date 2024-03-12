a0410d94e28e40b573529787fc464d0b14370353
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-12 18:42:58 +0100

    master branch name configuration

--- src/main/java/com/durys/jakub/configurationconsole/github/GithubProperties.java
+++ src/main/java/com/durys/jakub/configurationconsole/github/GithubProperties.java
@@ -9,12 +9,14 @@ class GithubProperties {
     private String password;
     private String repository;
     private String token;
+    private String masterBranchName;
 
-    GithubProperties(String username, String password, String repository, String token) {
+    GithubProperties(String username, String password, String repository, String token, String masterBranchName) {
         this.username = username;
         this.password = password;
         this.repository = repository;
         this.token = token;
+        this.masterBranchName = masterBranchName;
     }
 
     String username() {
@@ -31,5 +33,9 @@ class GithubProperties {
 
     String token() {
         return token;
+    }
+
+    String masterBranchName() {
+        return masterBranchName;
     }
 }
