131c4221a0ddfd3c600c083bf987dc7b4b25e725
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-12 20:09:31 +0100

    init configservice

---
+++ src/main/java/com/durys/jakub/configurationconsole/configuration/ConfigurationService.java
@@ -0,0 +1,26 @@
+package com.durys.jakub.configurationconsole.configuration;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class ConfigurationService {
+
+    private final BranchFactory branchFactory;
+    private final ContentFactory contentFactory;
+
+    ConfigurationService(BranchFactory branchFactory, ContentFactory contentFactory) {
+        this.branchFactory = branchFactory;
+        this.contentFactory = contentFactory;
+    }
+
+    public void changeConfiguration(String subsystem, String client, String property, String value) throws IOException {
+
+        String branchName = branchFactory.retrieveClientBranchName(client);
+
+
+
+    }
+
+}
