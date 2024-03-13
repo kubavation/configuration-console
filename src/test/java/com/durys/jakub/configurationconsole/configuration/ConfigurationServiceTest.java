00083da5e21476752095979d81d4d53fa0884537
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-13 18:16:27 +0100

    tests

---
+++ src/test/java/com/durys/jakub/configurationconsole/configuration/ConfigurationServiceTest.java
@@ -0,0 +1,33 @@
+package com.durys.jakub.configurationconsole.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ConfigurationServiceTest {
+
+    @Autowired
+    private ConfigurationService configurationService;
+
+    @Test
+    void shouldChangeConfiguration() throws IOException {
+
+        assertDoesNotThrow(() -> configurationService.changeConfiguration("cm-company-management", "TEST1", "val=123"));
+
+    }
+
+    @Test
+    void shouldLoadContent() throws IOException {
+
+        configurationService.changeConfiguration("cm-company-management", "TEST1", "val=123");
+
+        String content = configurationService.loadConfiguration("test123", "TEST1");
+
+        assertEquals("val=123", content);
+    }
+}
