9134fa673759d5efb5b90875d1b3182ee699bd82
kubavation <37585627+kubavation@users.noreply.github.com>
2024-03-14 19:32:05 +0100

    removed unused properties

--- src/main/java/com/durys/jakub/configurationconsole/github/GithubProperties.java
+++ src/main/java/com/durys/jakub/configurationconsole/github/GithubProperties.java
@@ -5,27 +5,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("git")
 class GithubProperties {
 
-    private final String username;
-    private final String password;
     private final String repository;
     private final String token;
     private final String masterBranchName;
 
-    GithubProperties(String username, String password, String repository, String token, String masterBranchName) {
-        this.username = username;
-        this.password = password;
+    GithubProperties( String repository, String token, String masterBranchName) {
         this.repository = repository;
         this.token = token;
         this.masterBranchName = masterBranchName;
     }
 
-    String username() {
-        return username;
-    }
-
-    String password() {
-        return password;
-    }
 
     String repository() {
         return repository;
